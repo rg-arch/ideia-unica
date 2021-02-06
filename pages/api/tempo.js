2f479d6369aa9d07d653ab7999cabf9a0b3f1258
Ronaldo Gomes <ronaldo.gomes@gmx.com>
2021-02-05 23:14:17 -0300

    primeira pagina

---
+++ pages/api/tempo.js
@@ -0,0 +1,10 @@
+function tempo(request, response) {
+      const dynamicDate = new Date();
+      
+      response.json({
+        date: dynamicDate.toUTCString(),
+        info: 'Ronaldo'
+      });
+}
+
+export default tempo;
