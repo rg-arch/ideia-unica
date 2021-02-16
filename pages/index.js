b6d77baeeacd4515cc2ba00b3e386316b4d7f875
Ronaldo Gomes <ronaldo.gomes@gmx.com>
2021-02-16 20:38:15 -0300

    Ronaldo 13

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,25 @@ function Home() {
         <div>
             <h2>Home Ronaldo Arthur e Gabriel -- Demetrius</h2>
             <Contador />
+            <IbgeSP />
             <h2>Sobre</h2>
                 <Link href='/sobre'>
                     <a>Acessar página sobre</a>
                 </Link>
+                
         </div>
     )
 }
 
+
+async function IbgeSP(request, response ) {
+    const IbgeResponse = await fetch ('https://servicodados.ibge.gov.br/api/v1/localidades/estados/SP');
+    const IbgeResponseJson = await IbgeResponse.json;
+    <div>
+        {IbgeResponseJson.nome}
+    </div>
+        
+}
 function Contador() {
 
     const [contador, setContador] = useState(1);
